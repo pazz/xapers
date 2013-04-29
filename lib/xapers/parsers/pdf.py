bd5b1e1911436499422f5f782b5d84f30b1853be
Patrick Totzke <patricktotzke@gmail.com>
2013-04-29 16:19:36 +0100

    new pdf to text parser
    this replaces the pdf2tx-subprocess based
    routine that extracts text from a given pdf file
    by one that is entirely written in python and based
    on pdfminer.
    
    This will later also enable us to react to
    "no text extractable" exceptions properly

--- lib/xapers/parsers/pdf.py
+++ lib/xapers/parsers/pdf.py
@@ -1,13 +1,27 @@
-def parse_file(path):
-    from subprocess import Popen, check_output, PIPE, CalledProcessError
-    cmd = ['pdftotext', path, '-']
-    #cmd = ['pdf2txt', path]
-    # FIXME: figure out how to trap errors better
-    text = check_output(' '.join(cmd), shell=True, stderr=open('/dev/null','w'))
-    #p = Popen(' '.join(cmd), stdout=PIPE, shell=True)
-    #text = p.communicate()[0]
-    # FIXME: do something here?
-    # if p.wait() != 0:
-    #     raise IOerror
+from StringIO import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
 
-    return text
+
+def pdf2txt(fp, password='', outcodec='utf-8', caching=True):
+    """
+    this does what's on the tin.
+    possibly raises `pdfminer.pdfinterp.PDFTextExtractionNotAllowed`
+    """
+    # no idea what this does
+    laparams = LAParams()
+    rsrcmgr = PDFResourceManager(caching=caching)
+
+    outfp = StringIO()
+    device = TextConverter(rsrcmgr, outfp, codec=outcodec, laparams=laparams)
+    process_pdf(rsrcmgr, device, fp, set(), maxpages=0, password=password,
+                caching=caching, check_extractable=True)
+    device.close()
+    txt = outfp.getvalue()
+    outfp.close()
+    return txt
+
+
+def parse_file(path):
+    return pdf2txt(open(path))
