579cfc602db41af815a202e56f9b22138e896761
Patrick Totzke <patricktotzke@gmail.com>
2013-05-09 20:09:13 +0100

    make use of subprocess helper in pdf parser

--- lib/xapers/parsers/pdf.py
+++ lib/xapers/parsers/pdf.py
@@ -1,11 +1,13 @@
-from ..parser import ParserBase
+from ..parser import ParserBase, ParseError
+from helper import call_cmd
 
-from subprocess import check_output
 
 class Parser(ParserBase):
     def extract(self):
-        cmd = ['pdftotext', self.path, '-']
+        out, err, rval = call_cmd(['pdftotext', self.path, '-'])
 
-        text = check_output(cmd, stderr=open('/dev/null','w'))
+        if rval != 0:
+            msg = 'pdftotext returned with exit code %d.\n%s' % (rval, err)
+            raise ParseError(msg)
 
-        return text
+        return out
