5091a429aa53d4d535dd826bce4bbd016c320f8d
Patrick Totzke <patricktotzke@gmail.com>
2013-05-11 12:24:05 +0100

    use ps2txt to parse postscript files

--- lib/xapers/parsers/__init__.py
+++ lib/xapers/parsers/__init__.py
@@ -4,6 +4,7 @@ from .helper import magic_mimetype, magic_encoding
 
 DOCUMENT_PARSERS = {
     'application/pdf': SubprocessDocParser(['pdftotext', '{PATH}', '-']),
+    'application/postscript': SubprocessDocParser(['ps2txt', '{PATH}']),
 }
 
 
@@ -14,7 +15,7 @@ def parse_file(path):
     except OSError as e:
         raise ParseError('Could not determine mimetype for %s:\n' % (path, e))
 
-    if mimetype == 'application/pdf':  # prevent this for anything not pdf for now
+    if mimetype in DOCUMENT_PARSERS:
         parser = DOCUMENT_PARSERS[mimetype]
         try:
             text = parser.extract_text(path)  #TODO: give encoding to parser as hint
